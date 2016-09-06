3ab0f5e3dc5cfad640dca0c1e3c61c81e615e5dc
Freeman ke <guang810828@gmail.com>
2016-09-07 07:47:00 +0800

    修改通过执行命令的的方式添加IP地址映射

--- r1.sh
+++ r1.sh
@@ -3,13 +3,17 @@
 #-----------------------------------------------------
 #根据实际需要更改COOKIE,DEFAULT_USER, DEFAULT_PASS参数
 #一个集群内的所有节点这三个参数需要一样
-NODE1=r1
-NODE2=r2
-NODE3=r3
+
+NODE1_IP=192.168.10.1
+NODE2_IP=192.168.10.2
+NODE2_IP=192.178.10.3
 COOKIE='NICAYNNZGMAHWQLRVQQU'
 DEFAULT_USER=admin
 DEFAULT_PASS=admin
-DATA_PATH=/home/freeman/rabbitmq
+DATA_PATH=/var/rabbitmq
+NODE1=r1
+NODE2=r2
+NODE3=r3
 
 echo "如果容器"$NODE1"已启动，停止并重新启动"
 docker rm -f $NODE1
@@ -21,7 +25,6 @@ docker run -d \
 -p 15672:15672 \
 -p 25672:25672 \
 -p 4369:4369 \
--v /etc/hosts:/etc/hosts \
 -v $DATA_PATH:/var/lib/rabbitmq \
 -e RABBITMQ_DEFAULT_USER=$DEFAULT_USER \
 -e RABBITMQ_DEFAULT_PASS=$DEFAULT_PASS \
@@ -32,5 +35,9 @@ rabbitmq:3.6.1-management
 
 echo "等待"$NODE1"容器就绪..."
 sleep 5
-echo "设置高可用策略"
+echo "添加节点地址映射到/etc/hosts文件..."
+docker exec $NODE1 bash -c 'echo "$NODE2_IP $NODE2" >> /etc/hosts'
+docker exec $NODE1 bash -c 'echo "$NODE3_IP $NODE3" >> /etc/hosts'
+
+echo "设置高可用策略..."
 docker exec $NODE1 rabbitmqctl set_policy HA '^(?!amq\.).*' '{"ha-mode": "all"}'
--- r3.sh
+++ r3.sh
@@ -3,13 +3,17 @@
 #-----------------------------------------------------
 #根据实际需要更改COOKIE,DEFAULT_USER, DEFAULT_PASS参数
 #一个集群内的所有节点这三个参数需要一样
-NODE1=r1
-NODE2=r2
-NODE3=r3
+
+NODE1_IP=192.168.10.1
+NODE2_IP=192.168.10.2
+NODE2_IP=192.178.10.3
 COOKIE='NICAYNNZGMAHWQLRVQQU'
 DEFAULT_USER=admin
 DEFAULT_PASS=admin
-DATA_PATH=/home/freeman/rabbitmq
+DATA_PATH=/var/rabbitmq
+NODE1=r1
+NODE2=r2
+NODE3=r3
 
 echo "如果容器"$NODE3"已启动，停止并重新启动"
 docker rm -f $NODE3
@@ -21,10 +25,9 @@ docker run -d \
 -p 15672:15672 \
 -p 25672:25672 \
 -p 4369:4369 \
--v /etc/hosts:/etc/hosts \
 -v $DATA_PATH:/var/lib/rabbitmq \
--e RABBITMQ_DEFAULT_USER=admin \
--e RABBITMQ_DEFAULT_PASS=admin \
+-e RABBITMQ_DEFAULT_USER=$DEFAULT_USER \
+-e RABBITMQ_DEFAULT_PASS=$DEFAULT_PASS \
 -e RABBITMQ_NODENAME=$NODE3 \
 -e RABBITMQ_ERLANG_COOKIE=$COOKIE \
 --hostname $NODE3 \
@@ -32,7 +35,12 @@ rabbitmq:3.6.1-management
 
 echo "等待容器就绪"
 sleep 3
-echo "加入集群"$NODE1"@"$NODE1
+
+echo "添加节点地址映射到/etc/hosts文件..."
+docker exec $NODE3 bash -c 'echo "$NODE2_IP $NODE2" >> /etc/hosts'
+docker exec $NODE3 bash -c 'echo "$NODE3_IP $NODE3" >> /etc/hosts'
+
+echo "加入集群"$NODE1"@"$NODE1"..."
 docker exec $NODE3 rabbitmqctl stop_app
 docker exec $NODE3 rabbitmqctl join_cluster --ram $NODE1@$NODE1
 docker exec $NODE3 rabbitmqctl start_app
